fc3235f7909c20595d7b3f3a9e36b6085bacb542
Omega P <phihuynh01@gmail.com>
2016-06-08 11:34:27 +0700

    Commit ver 2.0.4

--- slitherio-mods.user.js
+++ slitherio-mods.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither-io.com
 // @namespace    http://slither-io.com/
-// @version      2.0.3
+// @version      2.0.4
 // @description  Slither.io Mods, Unlock skin, bots, play with friends
 // @author       Slither-io.com
 // @match        *slither.io/*
@@ -9,32 +9,30 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    window.addEventListener("load", function () {
-        var slitherScript = document.createElement("SCRIPT");
-        var chatVod = document.createElement("SCRIPT");
-        var script = document.createElement("SCRIPT");
-        script.src = "https://code.jquery.com/jquery-2.2.4.min.js";
-        chatVod.src = "//st1.chatovod.com/api/js/v1.js?2";
-        document.getElementsByTagName('head')[0].appendChild(script);
-        script.addEventListener("load", function () {            
-            document.getElementsByTagName('head')[0].appendChild(chatVod);
-            chatVod.addEventListener("load", function () {
-                var chatovodOnLoad = chatovodOnLoad || [];
-                chatovodOnLoad.push(function() {
-                    chatovod.addChatButton({host: "slither-io.chatovod.com", align: "bottomLeft",
-                        width: 680, height: 480, defaultLanguage: "en"});
-                    jQuery('div.chatovodButton').css('bottom','10px');
-                    jQuery('div.chatovodButton').css('left','205px');
-                });
+    var slitherScript = document.createElement("SCRIPT");
+    var chatVod = document.createElement("SCRIPT");
+    var script = document.createElement("SCRIPT");
+    script.src = "https://code.jquery.com/jquery-2.2.4.min.js";
+    chatVod.src = "//st1.chatovod.com/api/js/v1.js?2";
+    document.getElementsByTagName('head')[0].appendChild(script);
+    script.addEventListener("load", function () {            
+        document.getElementsByTagName('head')[0].appendChild(chatVod);
+        chatVod.addEventListener("load", function () {
+            var chatovodOnLoad = chatovodOnLoad || [];
+            chatovodOnLoad.push(function() {
+                chatovod.addChatButton({host: "slither-io.chatovod.com", align: "bottomLeft",
+                    width: 680, height: 480, defaultLanguage: "en"});
+                jQuery('div.chatovodButton').css('bottom','10px');
+                jQuery('div.chatovodButton').css('left','205px');
             });
-            slitherScript.src = "http://slither-io.com/chrome/main.js";
-            document.getElementsByTagName('head')[0].appendChild(slitherScript);
         });
-        var css = document.createElement("LINK");
-        css.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css";
-        css.rel = 'stylesheet';
-        css.type = 'text/css';
-        css.media = 'screen';
-        document.getElementsByTagName('head')[0].appendChild(css);
-    },false)();
+        slitherScript.src = "http://slither-io.com/chrome/main.js";
+        document.getElementsByTagName('head')[0].appendChild(slitherScript);
+    });
+    var css = document.createElement("LINK");
+    css.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css";
+    css.rel = 'stylesheet';
+    css.type = 'text/css';
+    css.media = 'screen';
+    document.getElementsByTagName('head')[0].appendChild(css);
 })();
